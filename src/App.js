63e00d42d6461c4e39486aec323f1671a4203ffa
wszelag <wsz3lag@gmail.com>
2020-12-09 15:45:26 +0100

    add styled-components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,19 @@
 import React, { useState } from "react";
 import { Form } from "./components/Form/Form";
 import { InfoWrapper } from "./components/InfoWrapper/InfoWrapper";
+import { ThemeProvider } from "styled-components";
+import { theme } from "./style/theme";
+import { GlobalStyle } from "./style/GlobalStyle";
 
 export const App = () => {
   const [cityData, setCityData] = useState({ empty: true });
   return (
     <>
-      <Form setCityData={setCityData} />
-      <InfoWrapper cityData={cityData} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyle />
+        <Form setCityData={setCityData} />
+        <InfoWrapper cityData={cityData} />
+      </ThemeProvider>
     </>
   );
 };
